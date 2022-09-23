0dce5725a10ef56de90608a0b6c47834f41092f7
Keshav Gurushankar <kgurushankar@users.noreply.github.com>
2022-09-22 20:31:24 -0400

    add test

--- scripts/run_solution.sh
+++ scripts/run_solution.sh
@@ -8,7 +8,6 @@ set -e
 # C++: .cpp
 # Python: .py
 pushd $1 
-pwd
 
 if [[ -f "solution.c" ]];  then
     gcc solution.c -o solution 
@@ -27,16 +26,15 @@ elif [[ -f "solution.cpp" ]]; then
     diff --strip-trailing-cr $filename/test.out $filename/stdout
     done
 elif [[ -f "solution.py" ]]; then
-    
      for filename in test_cases/*
         do
     [ -d "$filename" ] || continue # failsafe
     python3 solution.py < $filename/stdin > $filename/test.out
     diff --strip-trailing-cr $filename/test.out $filename/stdout
     done
-    
 else
     echo "No solution file found"
+    exit 1
 fi
 
 popd
